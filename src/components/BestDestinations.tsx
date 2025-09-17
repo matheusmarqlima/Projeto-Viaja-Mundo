0e144b280b0bb57ae1610b2cb383f9a5d2b6f98f
Matheus Lima <lima8matheus@gmail.com>
2025-09-17 11:52:26 -0300

    Style: feito estilo dos cards

--- src/components/BestDestinations.tsx
+++ src/components/BestDestinations.tsx
@@ -2,10 +2,10 @@ import DestinationCard from "./DestinationCard";
 
 const BestDestinations = () => {
   return (
-    <section className="py-[64px] ">
-      <h2 className="text-4xl font-bold text-center">Melhores Destinos</h2>
-      <p>Explore os destinos mais procurados pelos viajantes do mundo todo</p>
-      <div>
+    <section className="py-[64px] flex flex-col items-center justify-center  ">
+      <h2 className="text-4xl font-bold text-center text-slate-950 mb-4">Melhores Destinos</h2>
+      <p className="text-center text-slate-500 py-[16px] px-[8px]">Explore os destinos mais procurados pelos viajantes do mundo todo</p>
+      <div className="flex flex-col gap-8">
         <DestinationCard />
         <DestinationCard />
         <DestinationCard />
--- src/components/DestinationCard.tsx
+++ src/components/DestinationCard.tsx
@@ -4,25 +4,25 @@ import Star from "../assets/star.svg?react";
 
 const DestinationCard = () => {
   return (
-    <div className="py-[64px] border-t-yellow-300">
-      <div>
-        <img src={machuPicchu} alt="Machu Picchu" />
-        <div>Aventura</div>
+    <div className="w-[396px] rounded-[12px] shadow-xl relative ">
+      <div className="">
+        <img src={machuPicchu} alt="Machu Picchu" className="rounded-t-2xl relative"/>
+        <div className="absolute top-2 left-4 bg-amber-500 rounded-xl py-[3px] px-[11px]">Aventura</div>
       </div>
-      <div>
-        <div>
-          <h3>Machu Picchu</h3>
-          <div>
+      <div className="mx-[15px] pb-[20px] flex flex-col gap-[8px]"> 
+        <div className="flex justify-between pt-[16px]">
+          <h3 className="text-xl font-semibold ">Machu Picchu</h3>
+          <div className="flex items-center text-slate-500">
             <Star /> 4.9
           </div>
         </div>
-        <div>
+        <div className="flex items-center gap-1 text-slate-500">
           <MapPin className="h-5 w-5" />
           Peru
         </div>
-        <div>
-          <span>A partir de</span>
-          <span>R$ 2.500</span>
+        <div className="flex justify-between ">
+          <span className="text-slate-500">A partir de </span>
+          <span className="text-sky-500 font-bold">R$ 2.500</span>
         </div>
       </div>
     </div>
