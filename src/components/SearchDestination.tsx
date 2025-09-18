029e5730ec84a3ca60cf7bd49e4ca0bb63cff73f
Matheus Lima <lima8matheus@gmail.com>
2025-09-17 22:16:40 -0300

    Style: arrendodado a borda transparente o SearchDestination

--- src/components/SearchDestination.tsx
+++ src/components/SearchDestination.tsx
@@ -5,7 +5,7 @@ import MapPin from "../assets/mappin.svg?react";
 
 const SearchDestination = () => {
   return (
-    <div className="backdrop-blur-md flex flex-col gap-5 p-6 bg-white/10 rounded-3x1 ">
+    <div className="backdrop-blur-md flex flex-col gap-5 p-6 bg-white/10 rounded-[10px] ">
       <InputText icon={<MapPin />} placeholder="Para onde você quer ir?" />
       <Button className="bg-sky-500 flex justify-center h-[48px] w-full rounded-[10px] items-center" icon={<SearchIcon />}>Buscar</Button>
     </div>
