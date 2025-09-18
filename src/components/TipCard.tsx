0f0bdd970985ccd3a67822f2cac4202033d9d3ac
Matheus Lima <lima8matheus@gmail.com>
2025-09-17 22:07:54 -0300

    Style: estilizado TipCard e TravelTips

--- src/components/TipCard.tsx
+++ src/components/TipCard.tsx
@@ -1,19 +1,21 @@
-import type { PropsWithChildren } from "react";
+import React, { type PropsWithChildren } from "react";
 import Subtitle from "./Subtitle";
 
 type Props = {
-  icon: React.ReactNode;
+  icon: React.ReactElement<{ className?: string }>; // <-- Modificação para entrar os icones com className
   title: string;
 } & PropsWithChildren;
 
 const TipCard = ({ icon, title, children }: Props) => {
   return (
-    <div>
-      <div>
-        <div>{icon}</div>
-        <Subtitle>{title}</Subtitle>
+    <div className="bg-white rounded-xl p-6 border border-gray-100 flex flex-col gap-4 border-l-4 border-l-sky-500 shadow-sm">
+      <div className="flex items-center gap-4">
+        <div className="bg-sky-100 p-3 rounded-full flex items-center justify-center">
+          {React.cloneElement(icon, { className: "w-6 h-6 text-sky-600" })}
+        </div>
+        <Subtitle className="font-semibold text-lg">{title}</Subtitle>
       </div>
-      <p>{children}</p>
+      <p className="text-slate-500">{children}</p>
     </div>
   );
 };
--- src/components/TravelTips.tsx
+++ src/components/TravelTips.tsx
@@ -2,19 +2,20 @@ import BulbIcon from "../assets/bulb.svg?react";
 import WifiIcon from "../assets/wifi.svg?react";
 import ForkKnifeIcon from "../assets/forkknife.svg?react";
 import CameraIcon from "../assets/camera.svg?react";
-
 import TipCard from "./TipCard";
 
 const TravelTips = () => {
   return (
-    <section>
-      <div>
-        <h2>Dicas de Viagem</h2>
-        <p>
+    <div className="p-4 sm:p-8 bg-white">
+      <section className="bg-blue-50 max-w-4xl mx-auto rounded-2xl py-12 px-4 sm:px-8">
+        <h2 className="text-4xl font-bold text-center text-slate-950 mb-4">
+          Dicas de Viagem
+        </h2>
+        <p className="text-center text-slate-500 max-w-xl mx-auto mb-12">
           Conselhos práticos de viajantes experientes para tornar sua jornada
           ainda melhor
         </p>
-        <div>
+        <div className="max-w-lg mx-auto flex flex-col gap-6">
           <TipCard icon={<BulbIcon />} title="Dica de Ouro">
             Sempre faça uma cópia digital de seus documentos e guarde na nuvem
             para emergências.
@@ -24,16 +25,17 @@ const TravelTips = () => {
             e roaming.
           </TipCard>
           <TipCard icon={<ForkKnifeIcon />} title="Gastronomia Local">
-            Experimente a comida de rua local, mas pesquise lugares recomendados
-            por moradores.
+            Experimente a comida de rua local, mas pesquise lugares
+            recomendados por moradores.
           </TipCard>
           <TipCard icon={<CameraIcon />} title="Fotografias">
             Acorde cedo para fotografar pontos turísticos sem multidões e com
             luz natural melhor.
           </TipCard>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
+
 export default TravelTips;
